89abe82b217f953bc5d1a8194ca37bc2fb867264
nutjeep <anajibmuhammad@gmail.com>
2022-01-10 20:49:05 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Toggle Menu for Mobile View
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function() {
+    nav.classList.toggle('slide');
+});
+
+
+// Stiky Navigation Bar
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("stiky", window.scrollY > 10);
+})
+
+// Jquery Easing - Smooth Scrollong Page
+$('.scrolling-page').on('click', function(e) { 
+    
+    var tujuan = $(this).attr('href');
+    var elemenTujuan = $(tujuan);
+
+    $('html,body').animate({
+        scrollTop: elemenTujuan.offset().top
+    }, 1200, 'easeInOutExpo');
+
+    e.preventDefault();
+});
+
+// Image Lightbox
+function toggle(){
+    var blur = document.getElementById('education');
+    blur.classList.toggle('blur-effect');
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('blur');
+}
